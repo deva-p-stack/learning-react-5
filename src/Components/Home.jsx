7b7f5f66e7449689f63b0201869f51c4f6d82a6c
deva-p-stack <devap677@gmail.com>
2026-02-25 17:30:16 +0530

    FEAt: limit  ;

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,20 +3,27 @@ import { useEffect, useState } from "react";
 function Home() {
   const [users, setUsers] = useState([]);
 const [searchTerm, setSearchTerm] = useState("");
-const [count ,setCount] = useState("");
+const [limit, setLimit] = useState(5);
+// const [count ,setCount] = useState("");
+
+ const limitChange = (e) => {
+    setLimit(Number(e.target.value)); 
+  };
 
   useEffect(() => {
     async function fetchCustomer() {
       const response = await fetch(
             
-             `https://mimic-server-api.vercel.app/bills?q=${searchTerm}&_limit=6`
+             `https://mimic-server-api.vercel.app/bills?q=${searchTerm}&_limit=${limit}`
+
+            //  ${limit}
         
       );
       const data = await response.json();
       setUsers(data);
     }
     fetchCustomer();
-  }, [searchTerm]);
+  }, [searchTerm,limit]);
 
   return (
     <div className="min-h-[100vh] bg-slate-100 p-6">
@@ -32,8 +39,17 @@ onChange={(e) => setSearchTerm(e.target.value)}
 </div>
 
       {/* bill counter */}
-      <div className="pb-5">
-        <span className="px-3 py-2 rounded-lg bg-white text-md" >Total Bill  - {users.length || "No bills"} </span>
+      <div className="pb-5 flex justify-between">
+        <div className="px-3 py-2 rounded-lg bg-white text-md" >Total Bill  - {users.length || "No bills"} </div>
+        <div className="flex ">
+            <select className="px-5 py-2 outline-none" value={limit} onChange={limitChange} name="limits" id="limits">
+              <option value="5">5</option>
+              <option value="10">10</option>
+              <option value="15">15</option>
+              <option value="20">20</option>
+            </select>
+           </div>
+          
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
