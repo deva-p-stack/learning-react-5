699e812e1afaac252b1c3ddfb791a4ff777e21b8
deva-p-stack <devap677@gmail.com>
2026-02-25 13:06:13 +0530

    UI: improve the card ;

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,18 +3,20 @@ import { useEffect, useState } from "react";
 function Home() {
   const [users, setUsers] = useState([]);
 const [searchTerm, setSearchTerm] = useState("");
+const [count ,setCount] = useState("");
 
   useEffect(() => {
     async function fetchCustomer() {
       const response = await fetch(
-        "https://mimic-server-api.vercel.app/bills"
+            
+             `https://mimic-server-api.vercel.app/bills?q=${searchTerm}&_limit=6`
         
       );
       const data = await response.json();
       setUsers(data);
     }
     fetchCustomer();
-  }, []);
+  }, [searchTerm]);
 
   return (
     <div className="min-h-[100vh] bg-slate-100 p-6">
@@ -29,14 +31,17 @@ onChange={(e) => setSearchTerm(e.target.value)}
 />
 </div>
 
-
+      {/* bill counter */}
+      <div className="pb-5">
+        <span className="px-3 py-2 rounded-lg bg-white text-md" >Total Bill  - {users.length || "No bills"} </span>
+      </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
        
        {/* map loop each data  */}
         {users.map((bill) => (
           
-          <div  className="bg-white shadow-lg rounded-2xl p-6  transition-all ease-in-out hover:-translate-y-1 duration-300">
+          <div  key={bill.id}  className="bg-white shadow-lg rounded-2xl p-6  transition-all ease-in-out hover:-translate-y-1 duration-300">
             {/* cutomser details */}
             <div className="mb-2 flex justify-between">
               <h2 className="text-2xl font-semibold text-slate-800">
